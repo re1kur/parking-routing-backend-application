8455a7c197f1ff16282282c0a6100b0ad839cd8a
re1kur <re1kur@yahoo.com>
2025-07-12 18:42:28 +0700

    full docker-compose of project.
    fixes, new dockerfiles, composes, refactoring, .gitignore files

--- file-service/src/main/java/re1kur/fs/FileServiceApplication.java
+++ file-service/src/main/java/re1kur/fs/FileServiceApplication.java
@@ -4,14 +4,10 @@ import io.awspring.cloud.autoconfigure.s3.S3AutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.File;
-
 @SpringBootApplication(exclude = S3AutoConfiguration.class)
 public class FileServiceApplication {
 
     public static void main(String[] args) {
-        System.out.println(System.getenv("PATH"));
-        System.out.println(new File("/home/re1kur/Projects/parking-routing-backend-app/file-service/dev-compose.yml").exists());
         SpringApplication.run(FileServiceApplication.class, args);
     }
 
